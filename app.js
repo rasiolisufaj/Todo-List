47000a679b03200d461629f7ef1deff334f1019e
Rasiol Isufaj <rasiolisufaj47@gmail.com>
2022-08-20 12:11:44 +0200

    searching and filtering

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // Adding todos
 const addForm = document.querySelector(".add");
 const todosElement = document.querySelector(".todos");
+const searchFieldElement = document.querySelector("#searchField");
+const todosChildren = todosElement.children;
 
+// Function to generate li template
 const generateTemplate = (todo) => {
   const li = document.createElement("li");
   const span = document.createElement("span");
@@ -20,7 +23,7 @@ const generateTemplate = (todo) => {
 
 addForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const todo = addForm.add.value.trim();
+  const todo = addForm.add.value.trim().toLowerCase();
   todosElement.append(generateTemplate(todo));
   e.target.reset();
 });
@@ -29,5 +32,19 @@ addForm.addEventListener("submit", (e) => {
 todosElement.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete")) {
     e.target.parentElement.remove();
+  }
+});
+
+// Searching & Filtering Todos
+searchFieldElement.addEventListener("keyup", (e) => {
+  const keyword = searchFieldElement.value.toLowerCase();
+  console.log(todosChildren.length);
+  for (let index = 0; index < todosChildren.length; index++) {
+    const liElement = todosChildren.item(index);
+    if (!liElement.children[0].innerText.includes(keyword)) {
+      liElement.classList.add("d-none");
+    } else {
+      liElement.classList.remove("d-none");
+    }
   }
 });
